89fca842f654e10a70e35848bfe26d886233dd4e
prashantbhoite123 <pbhoite985@gmail.com>
2024-08-01 20:57:19 +0530

    complete object practice

---
+++ 02Basic/object.ts
@@ -0,0 +1,22 @@
+// const hello = {
+//   name: "prashant",
+//   age: 20,
+//   isPaid: false,
+// }
+
+// function createObject({ name: string, isPaid: boolean }) {
+//   return
+// }
+
+// let newUser = { name: "prashant", isPaid: false, email: "prashu@gmail.com" }
+// createObject(newUser)
+
+// function createCourse(): { name: string; price: number } {
+//   return { name: "javascript", price: 399 }
+// }
+
+
+
+
+
+export {}
