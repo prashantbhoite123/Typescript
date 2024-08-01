f847d62b76263d7614d1f54298f460f678458ee8
prashantbhoite123 <pbhoite985@gmail.com>
2024-08-01 15:19:35 +0530

    complete  typeScript practice

--- 02Basic/function.ts
+++ 02Basic/function.ts
@@ -1,30 +1,62 @@
+// const hello = (num: number) => {
 
+//     // return num + 2
+// return "hello How You Whats Matter"
+// }
 
-const hello = (num: number) => {
-    
-    // return num + 2
-return "hello How You Whats Matter"
+// function getUpper(val:string) {
+//     val .toUpperCase()
+//     return
+// }
+
+// function signUpUser(name:string, email:number, isAdmin:boolean) {
+//     return { name, email, isAdmin }
+// }
+
+// signUpUser("hello Prashant", 2, true)
+
+// let loginUser = (name:any,email:any,password:any,isPaid:boolean) => {
+//     return {name,email,password}
+// }
+
+// function getValue(num:number):boolean{
+//     if (num >5) {
+//         return true
+//     }
+//     return "200 ok"
+// }
+
+// loginUser("Datta Shete","Datta@gmail.com","datta",true)
+// getUpper("49")
+// hello(300)
+
+const getValue = (s: string): string => {
+  return ""
 }
 
-function getUpper(val:string) {
-    val .toUpperCase()
-    return
-}
+// const heros:Array<string> = ["thor","spiderman","shaktiman","ironman"]
+// // // const heros = [1, 2, 3, 4, 5, 6]
 
-function signUpUser(name:string, email:number, isAdmin:boolean) {
-    return { name, email, isAdmin }
-}
+// // heros.map((heros): string => {
+// //   return `hero is ${heros}`
+// // })
+
+// function consoleError(errorMsg: string){
+//     return 1
+// }
+
+// function handelEror(handelErorr:string){
+// throw new Error(handelErorr)
+// }
 
 
-signUpUser("hello Prashu how are You and What is This How Are Your", 2, true)
+const x: [string, number] = ["prashant", 1234];
 
-let loginUser = (name:any,email:any,password:any,isPaid:boolean) => {
-    return {name,email,password}
-}
- 
+let notSure: unknown = 4
 
-loginUser("Datta Shete","Datta@gmail.com","datta",true)
-getUpper("49")
-hello(300)
 
-export{ }
+
+
+
+
+export {}
