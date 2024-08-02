d086949d8dbbd1f11b32cfa1577a9b57e503a618
prashantbhoite123 <pbhoite985@gmail.com>
2024-08-02 20:42:22 +0530

    complete array practice

---
+++ 02Basic/MyArray.ts
@@ -0,0 +1,43 @@
+// const supperHero: Array<string>  | Array<number> = [89487]
+
+// const heroPower: number[] = []
+// const heroinPower: boolean[] = []
+
+// heroinPower.push(true)
+// heroPower.push(9021018032)
+
+type User = {
+  name: string
+  isActive: boolean
+}
+
+const allUser: User[] = []
+
+const MlMOdels: number[][] = [
+  [255.255, 255],
+  [43865, 435, 435, 435],
+]
+
+allUser.push({ name: "prashant", isActive: true })
+
+type SingUser = {
+  name: string
+  email: string
+  password: string
+  isAdmin: boolean
+  isPaid: boolean
+}
+
+// const completeSign: SingUser = {
+//   name: "prashant",
+//   email: "prashu@gmail.com",
+//   password: "232343",
+//   isAdmin: true,
+//   isPaid: false,
+// }
+
+
+const isUser: SingUser[] = [
+]
+
+
